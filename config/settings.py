0e89c61bedb7c9d28838b15d1c58f6118bfaa096
behruz12005 <behruzqalqonov@gmail.com>
2023-06-04 10:52:25 +0500

    about

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -118,8 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- youtube/views.py
+++ youtube/views.py
@@ -6,89 +6,92 @@ import os
 from django.shortcuts import render, get_object_or_404
 def Home(request):
     data = YouTobeModel.objects.all()
+    try:
+        if request.method == 'POST':
+            video_link = request.POST['videoLink']
+            video_id = video_link.split('=')[1]
+            video_id = video_id.split('&')[0]
 
-    if request.method == 'POST':
-        video_link = request.POST['videoLink']
-        video_id = video_link.split('=')[1]
-        video_id = video_id.split('&')[0]
+            api_key = 'AIzaSyCq7Q0ELgdp9VjTcPyytE9dyWJg8rXEo5I'
+            youtube = build('youtube', 'v3', developerKey=api_key)
+            video_response = youtube.videos().list(
+                part='snippet,statistics,contentDetails',
+                id=video_id
+            ).execute()
 
-        api_key = 'AIzaSyCq7Q0ELgdp9VjTcPyytE9dyWJg8rXEo5I'
-        youtube = build('youtube', 'v3', developerKey=api_key)
-        video_response = youtube.videos().list(
-            part='snippet,statistics,contentDetails',
-            id=video_id
-        ).execute()
+            api_service_name = "youtube"
+            api_version = "v3"
+            youtube = googleapiclient.discovery.build(
+                api_service_name, api_version, developerKey=api_key)
 
-        api_service_name = "youtube"
-        api_version = "v3"
-        youtube = googleapiclient.discovery.build(
-            api_service_name, api_version, developerKey=api_key)
-
-        api_request = youtube.commentThreads().list(
-            part="snippet",
-            videoId=video_id,
-            maxResults=100
-        )
-        response = api_request.execute()
-
-        comments = response["items"]
-
-        for comment in comments:
-            # Komment matnini olishish
-            text1 = comment["snippet"]["topLevelComment"]["snippet"]["textDisplay"]
-
-            # Komment sanasini olishish
-            date = comment["snippet"]["topLevelComment"]["snippet"]["publishedAt"]
-
-            # Avtorni olishish
-            author = comment["snippet"]["topLevelComment"]["snippet"]["authorDisplayName"]
-
-            # Avtor haqida qo'shimcha ma'lumotlarni olishish
-            author_info = comment["snippet"]["topLevelComment"]["snippet"]["authorChannelId"]
-
-            # Avtor profil URL sini olishish
-            author_profile_url = "https://www.youtube.com/channel/" + author_info['value']
-
-            # Commentlar bilan Comment modelini to'ldirish
-            comment_model = Comment(
-                text_id=video_id,
-                text=text1,
-                date=date,
-                author=author,
-                author_info=author_info,
-                author_profile_url=author_profile_url
+            api_request = youtube.commentThreads().list(
+                part="snippet",
+                videoId=video_id,
+                maxResults=100
             )
-            comment_model.save()
+            response = api_request.execute()
 
-        video_info = video_response.get('items', [])
-        if video_info:
-            video_info = video_info[0]
-        else:
-            video_info = {}
+            comments = response["items"]
 
-        video_title = video_info.get('snippet', {}).get('title', '')
-        channel_title = video_info.get('snippet', {}).get('channelTitle', '')
-        description = video_info.get('snippet', {}).get('description', '')
-        view_count = video_info.get('statistics', {}).get('viewCount', '')
+            for comment in comments:
+                # Komment matnini olishish
+                text1 = comment["snippet"]["topLevelComment"]["snippet"]["textDisplay"]
 
-        content_model = YouTobeModel(
-            video_id=video_id,
-            channelTitle=channel_title,
-            title=video_title,
-            viewCount=int(view_count),
-            description=description
-        )
-        content_model.save()
+                # Komment sanasini olishish
+                date = comment["snippet"]["topLevelComment"]["snippet"]["publishedAt"]
 
-        return render(request, 'home.html', {
-            'video_title': video_title,
-            'channel_title': channel_title,
-            'description': description,
-            'view_count': view_count,
-            'data': data
-        })
+                # Avtorni olishish
+                author = comment["snippet"]["topLevelComment"]["snippet"]["authorDisplayName"]
 
-    return render(request, 'home.html', {'data': data})
+                # Avtor haqida qo'shimcha ma'lumotlarni olishish
+                author_info = comment["snippet"]["topLevelComment"]["snippet"]["authorChannelId"]
+
+                # Avtor profil URL sini olishish
+                author_profile_url = "https://www.youtube.com/channel/" + author_info['value']
+
+                # Commentlar bilan Comment modelini to'ldirish
+                comment_model = Comment(
+                    text_id=video_id,
+                    text=text1,
+                    date=date,
+                    author=author,
+                    author_info=author_info,
+                    author_profile_url=author_profile_url
+                )
+                comment_model.save()
+
+            video_info = video_response.get('items', [])
+            if video_info:
+                video_info = video_info[0]
+            else:
+                video_info = {}
+
+            video_title = video_info.get('snippet', {}).get('title', '')
+            channel_title = video_info.get('snippet', {}).get('channelTitle', '')
+            description = video_info.get('snippet', {}).get('description', '')
+            view_count = video_info.get('statistics', {}).get('viewCount', '')
+
+            content_model = YouTobeModel(
+                video_id=video_id,
+                channelTitle=channel_title,
+                title=video_title,
+                viewCount=int(view_count),
+                description=description
+            )
+            content_model.save()
+
+            return render(request, 'home.html', {
+                'video_title': video_title,
+                'channel_title': channel_title,
+                'description': description,
+                'view_count': view_count,
+                'data': data
+            })
+
+        return render(request, 'home.html', {'data': data})
+    except:
+        text = 'Yoroqsiz link berildi yoki Takrorlanga link kiritilidi ! Malumot saqlanmadi .'
+        return render(request, 'home.html', {'data': data,'text':text})
 
 
 
@@ -98,76 +101,3 @@ def comment(request, pk):
     comments = Comment.objects.filter(text_id=video_id)
     
     return render(request, 'comment.html', {'video_id': video_id, 'comments': comments})
-
-
-
-
-
-
-
-# def Commnet(request):
-#     data = Comment.objects.all()
-
-#     if request.method == 'POST':
-#         try:
-#             os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
-
-#             api_service_name = "youtube"
-#             api_version = "v3"
-#             DEVELOPER_KEY = "AIzaSyCq7Q0ELgdp9VjTcPyytE9dyWJg8rXEo5I"
-
-#             youtube = googleapiclient.discovery.build(
-#                 api_service_name, api_version, developerKey=DEVELOPER_KEY)
-
-#             video_id = "01sAkU_NvOY"
-
-#             request = youtube.commentThreads().list(
-#                 part="snippet",
-#                 videoId=video_id,
-#                 maxResults = 100
-#             )
-#             response = request.execute()
-
-#             comments = response["items"]
-#             # comment_count = len(comments)
-
-#             for comment in comments:
-#                 # Komment matnini olishish
-#                 text = comment["snippet"]["topLevelComment"]["snippet"]["textDisplay"]
-
-#                 # Komment sanasini olishish
-#                 date = comment["snippet"]["topLevelComment"]["snippet"]["publishedAt"]
-
-#                 # Avtorni olishish
-#                 author = comment["snippet"]["topLevelComment"]["snippet"]["authorDisplayName"]
-
-#                 # Avtor haqida qo'shimcha ma'lumotlarni olishish
-#                 author_info = comment["snippet"]["topLevelComment"]["snippet"]["authorChannelId"]
-
-#                 # Avtor profil URL sini olishish
-#                 author_profile_url = "https://www.youtube.com/channel/" + author_info['value']
-
-#             # Commentlar ro'yxatini olish
-
-#             # Commentlar bilan Comment modelini to'ldirish
-#                 comment_model = Comment(
-#                     text=text,
-#                     date=date,
-#                     author=author,
-#                     author_info=author_info,
-#                     author_profile_url=author_profile_url
-#                 )
-#                 comment_model.save()
-
-#             return render(request, 'home.html', {
-#                 'video_title': video_title,
-#                 'channel_title': channel_title,
-#                 'description': description,
-#                 'view_count': view_count,
-#                 'data': data
-#             })
-#         except:
-#             text = 'Sizda yaroqsiz link yoki takrorlangan link berdingiz. Malumot saqlanmadi !.'
-#             return render(request, 'home.html', {'data': data, 'text': text})
-
-#     return render(request, 'home.html', {'data': data})
